bbef6da2e81bd145e41b4c8d48cea38212b5e993
dmyronov <dmyronov@gmail.com>
2024-08-05 17:29:18 +0300

    should be OK

--- task02/src/lambdas/hello_world/handler.py
+++ task02/src/lambdas/hello_world/handler.py
@@ -14,45 +14,58 @@ class HelloWorld(AbstractLambda):
         """
         Explain incoming event here
         """
+        is_http = False
+        if ('requestContext' in event and 'http' in event['requestContext']):
+            _LOG.info(f'HTTP Request presetns')
+            is_http = True
         # todo implement business logic
-        """
-        path = event.get('rawPath')
-        method = event.get('requestContext', {}).get('http', {}).get('method')
+        if is_http:
+            _LOG.info(f'HTTP Request received')
+            path = event.get('rawPath')
+            method = event.get('requestContext', {}).get('http', {}).get('method')
+            _LOG.info(f'Received request with path: {path} and method: {method}')
 
-        headers = {
-            'Content-Type': 'application/json'
-        }
+            headers = {
+                'Content-Type': 'application/json'
+            }
 
-        if path == '/hello' and method == 'GET':
-            response_body = {
-                'statusCode': 200,
-                'message': 'Hello from Lambda'
-            }
-            _LOG.info('Returning successful response for /hello GET')
-            response = {
-                'statusCode': 200,
-                'headers': headers,
-                'body': json.dumps(response_body)
-            }
-            return response
+            if path == '/hello' and method == 'GET':
+                response_body = {
+                    'statusCode': 200,
+                    'message': 'Hello from Lambda'
+                }
+                _LOG.info('Returning successful response for /hello GET')
+                response = {
+                    'statusCode': 200,
+                    'headers': headers,
+                    'body': json.dumps(response_body)
+                }
+                return response
+            else:
+                error_message = f'Bad request syntax or unsupported method. Requested path: {path}, HTTP method: {method}'
+                _LOG.error(error_message)
+                response_body = {
+                    'statusCode': 400,
+                    'message': error_message
+                }
+                return {
+                    'statusCode': 400,
+                    'headers': headers,
+                    'body': json.dumps(response_body)
+                }
         else:
-            error_message = f'Bad request syntax or unsupported method. Requested path: {path}, HTTP method: {method}'
-            _LOG.error(error_message)
-            response_body = {
-                'statusCode': 400,
-                'message': error_message
-            }
-            return {
-                'statusCode': 400,
-                'headers': headers,
-                'body': json.dumps(response_body)
-            }
-            """
-        return 200
+            _LOG.info(f'HTTP Request not found')
+            return 200
     
 
 HANDLER = HelloWorld()
 
 
 def lambda_handler(event, context):
+    _LOG.info('Handler execution started')
+    _LOG.info('Event:')
+    _LOG.info(event)
+    _LOG.info('Context:')
+    _LOG.info(context)
     return HANDLER.lambda_handler(event=event, context=context)
+    _LOG.info('Handler execution completed')
